0225dd8ca1e32f7cf45d421a4bd52cacf755fad7
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2023-08-15 10:13:53 -0500

    Update config files, clean up codebase, and initialize prisma

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: true,
+  },
+  // https://nextjs.org/docs/pages/api-reference/next-config-js/images
+  images: {
+    // https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
+
+// Refernence: https://nextjs.org/docs/pages/api-reference/next-config-js
