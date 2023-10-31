35184bb6f6b20866c4fde5314c30d5364d6366a9
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2023-10-31 13:16:35 -0500

    Implement left sidebar for navigation

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,25 +1,28 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 const TopBar = () => {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">
-          Threads
-        </p>
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
       <div className="flex items-center gap-1">
         <div className="block md:hidden">
           <SignedIn>
             <SignOutButton>
-            <div className="flex cursor-pointer">
-              <Image src="/assets/logout.svg" alt="logout" width={24} height={24}/>
-            </div>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
             </SignOutButton>
           </SignedIn>
         </div>
@@ -27,19 +30,16 @@ const TopBar = () => {
         <OrganizationSwitcher
           appearance={{
             elements: {
-              OrganizationSwitcherTrigger:
-                "py-2 px4"
-            }
+              OrganizationSwitcherTrigger: "py-2 px4",
+            },
           }}
-          />
-
+        />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default TopBar
-
+export default TopBar;
 
 // References:
 // https://clerk.com/docs/components/unstyled/sign-out-button#sign-out-button
