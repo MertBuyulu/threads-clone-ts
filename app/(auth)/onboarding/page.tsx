16688e2f59d64e8d852c2182277cea4f97c1469b
Mert Buyulu <amertbyl@gmail.com>
2024-01-12 12:32:25 -0600

    Update dependencies and make navbar mobile responsive

--- app/(auth)/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Onboarding = async () => {
-  return (
-    <div className='head-text'>Onboarding</div>
-  )
-}
-
-export default Onboarding
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,9 @@
+'use server'
+
+import  { prisma } from "../prisma"
+
+// 1. Fetch user
+// 2. Update user
+// 3. Update user' posts
+// 4. Fetch users
+// 5. Get activity
