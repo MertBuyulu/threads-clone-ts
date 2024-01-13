7ba1ac00b69e6ae829689ed9af100da6ecee2e47
Mert Buyulu <amertbyl@gmail.com>
2024-01-12 19:15:55 -0600

    Set up uploadthing for file uploads

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,3 +7,7 @@ import  { prisma } from "../prisma"
 // 3. Update user' posts
 // 4. Fetch users
 // 5. Get activity
+
+
+// understand server actions:
+// https://blog.logrocket.com/diving-into-server-actions-next-js-14/#:~:text=js%20Server%20Actions%20are%20functions,mutating%20data%20from%20the%20client.
